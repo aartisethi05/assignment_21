89a679fe38381f91b742be5c929c99b37b9967db
Aarti Sethi <aartisethi8194@gmail.com>
2020-08-05 22:03:58 +0530

    'procfile'

--- resources/views/forgotPassword.blade.php
+++ resources/views/forgotPassword.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">  
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
